b015ccc5bb0bcf1b4d23c8a23fba6f17ea1b69d5
coonik <vuonglongxungba@gmail.com>
2020-07-08 11:32:52 +0700

    Section 1 - Video 7

--- PROJECT/my-first-app/src/app/app.module.ts
+++ PROJECT/my-first-app/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Khong import van dung duoc
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
