c1c72dfdf6d24c61315750eeaa89dbc752e56bac
coonik <vuonglongxungba@gmail.com>
2020-07-08 14:50:55 +0700

    Section 2 - Video 21

--- PROJECT/my-first-app/src/app/servers/servers.component.ts
+++ PROJECT/my-first-app/src/app/servers/servers.component.ts
@@ -3,7 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   template: '<p>Test</p>',
-  styleUrls: ['./servers.component.css']
+  // styleUrls: ['./servers.component.css']
+  styles: [
+    `
+    p {
+      color: yellow;
+    }
+    `
+  ]
 })
 export class ServersComponent implements OnInit {
 
