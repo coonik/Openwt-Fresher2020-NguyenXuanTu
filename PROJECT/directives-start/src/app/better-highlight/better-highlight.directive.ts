668924ab48c98a18f8248e586b40cba093f1425f
coonik <vuonglongxungba@gmail.com>
2020-07-10 15:52:09 +0700

    Section 6 - Video 98

--- PROJECT/directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ PROJECT/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,28 +1,37 @@
-import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding, Input, OnChanges, DoCheck } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective implements OnInit {
+export class BetterHighlightDirective implements OnInit, DoCheck {
   @HostBinding('style.textAlign')
+  @Input('appBetterHighlight')
+  positionInput: string = 'center';
+
   tAlign: string;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
     // this.renderer.setStyle(this.elRef.nativeElement,'text-align','center');
-    this.tAlign = 'center'
+    this.tAlign = this.positionInput;
+    // console.log(this.positionInput);
+
+  }
+
+  ngDoCheck() {
+    this.positionInput === 'left' ? this.positionInput = 'center' : this.positionInput === 'center' ? this.positionInput = 'right' : this.positionInput = 'left';
   }
 
   @HostListener('mouseenter')
   mouseover(eventData: Event){
     // this.renderer.setStyle(this.elRef.nativeElement,'text-align','left');
-    this.tAlign = "left";
+    this.tAlign = this.positionInput;
   }
 
   @HostListener('mouseleave')
   mouseleave(eventData: Event){
     // this.renderer.setStyle(this.elRef.nativeElement,'text-align','right');
-    this.tAlign = 'right'
+    this.tAlign = this.positionInput
   }
 }
