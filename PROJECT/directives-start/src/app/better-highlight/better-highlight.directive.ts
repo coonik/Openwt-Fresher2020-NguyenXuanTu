7b35838c069aecbf7dc8410895f6549e3e75cccb
coonik <vuonglongxungba@gmail.com>
2020-07-10 15:12:14 +0700

    Section 6 - Video 97

--- PROJECT/directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ PROJECT/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,23 +1,28 @@
-import { Directive, ElementRef, Renderer2, OnInit, HostListener } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.textAlign')
+  tAlign: string;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elRef.nativeElement,'text-align','center');
+    // this.renderer.setStyle(this.elRef.nativeElement,'text-align','center');
+    this.tAlign = 'center'
   }
 
   @HostListener('mouseenter')
   mouseover(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement,'text-align','left');
+    // this.renderer.setStyle(this.elRef.nativeElement,'text-align','left');
+    this.tAlign = "left";
   }
 
   @HostListener('mouseleave')
   mouseleave(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement,'text-align','right');
+    // this.renderer.setStyle(this.elRef.nativeElement,'text-align','right');
+    this.tAlign = 'right'
   }
 }
