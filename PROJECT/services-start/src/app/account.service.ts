88dc4956f685953ea05c937acce97447f9392939
coonik <vuonglongxungba@gmail.com>
2020-07-13 11:00:59 +0700

    Section 9 - Video 111

--- PROJECT/services-start/src/app/account.service.ts
+++ PROJECT/services-start/src/app/account.service.ts
@@ -1,3 +1,7 @@
+import { LoggingService } from "./logging.service";
+import { Injectable } from "@angular/core";
+
+@Injectable()
 export class AccountService {
   accounts = [
     {
@@ -14,6 +18,8 @@ export class AccountService {
     },
   ];
 
+  constructor(private loggingService: LoggingService) {}
+
   addAccount(name: string, status: string) {
     this.accounts.push({
       name,
@@ -23,5 +29,6 @@ export class AccountService {
 
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
--- PROJECT/services-start/src/app/app.component.ts
+++ PROJECT/services-start/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { AccountService } from "./account.service";
 import { Component, OnInit } from "@angular/core";
+import { LoggingService } from "./logging.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
-  providers: [AccountService],
+  providers: [AccountService, LoggingService],
 })
 export class AppComponent implements OnInit {
   accounts: { name: string; status: string }[];
