f4bf083e962af7bea4d440916808c1f62796f134
coonik <vuonglongxungba@gmail.com>
2020-07-13 10:32:59 +0700

    Section 9 - Video 108

--- PROJECT/services-start/src/app/account/account.component.ts
+++ PROJECT/services-start/src/app/account/account.component.ts
@@ -1,24 +1,28 @@
-import { LoggingService } from "./../logging.service";
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { AccountService } from "./../account.service";
+import { Component, Input } from "@angular/core";
+import { LoggingService } from "../logging.service";
 
 @Component({
   selector: "app-account",
   templateUrl: "./account.component.html",
   styleUrls: ["./account.component.css"],
-  providers: [LoggingService],
+  providers: [LoggingService, AccountService],
 })
 export class AccountComponent {
   @Input() account: { name: string; status: string };
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{
-    id: number;
-    newStatus: string;
-  }>();
-
-  constructor(private loggingService: LoggingService) {}
+  // @Output() statusChanged = new EventEmitter<{
+  //   id: number;
+  //   newStatus: string;
+  // }>();
+  constructor(
+    private loggingService: LoggingService,
+    private accountService: AccountService
+  ) {}
 
   onSetTo(status: string) {
-    this.statusChanged.emit({ id: this.id, newStatus: status });
+    // this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountService.updateStatus(this.id, status);
     console.log("A server status changed, new status: " + status);
   }
 }
