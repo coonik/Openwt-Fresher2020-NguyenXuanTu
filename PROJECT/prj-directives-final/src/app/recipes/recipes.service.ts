0f0f8638bb2963d9afb86032b9a8100a312f83e0
coonik <vuonglongxungba@gmail.com>
2020-07-13 15:05:48 +0700

    Section 10 - Video 121

--- PROJECT/prj-directives-final/src/app/recipes/recipes.service.ts
+++ PROJECT/prj-directives-final/src/app/recipes/recipes.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -8,12 +9,14 @@ export class RecipeService {
     new Recipe(
       "A Test Recipe",
       "This is simply a test",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+      [new Ingredient("apple", 12), new Ingredient("coconut", 2)]
     ),
     new Recipe(
       "Another Test Recipe",
       "This is simply a test",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+      [new Ingredient("salt", 12), new Ingredient("sweet", 22)]
     ),
   ];
 
