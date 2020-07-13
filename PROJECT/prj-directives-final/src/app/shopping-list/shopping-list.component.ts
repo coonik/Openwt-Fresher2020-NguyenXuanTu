f4fe0057ccfcf72173f632b932dc5c33b76ba4b1
coonik <vuonglongxungba@gmail.com>
2020-07-13 14:40:39 +0700

    Section 10 - Video 119

--- PROJECT/prj-directives-final/src/app/shopping-list/shopping-list.component.ts
+++ PROJECT/prj-directives-final/src/app/shopping-list/shopping-list.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { ShoppingListService } from "./shopping-list.service";
+import { Component, OnInit } from "@angular/core";
 
-import { Ingredient } from '../shared/ingredient.model';
+import { Ingredient } from "../shared/ingredient.model";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrls: ["./shopping-list.component.css"],
+  providers: [ShoppingListService],
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ];
+  ingredients: Ingredient[];
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit() {
+    this.ingredients = this.shoppingListService.ingredients;
   }
 
   onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
+    this.shoppingListService.addIngredient(ingredient);
   }
 }
--- PROJECT/prj-directives-final/src/app/shopping-list/shopping-list.service.ts
+++ PROJECT/prj-directives-final/src/app/shopping-list/shopping-list.service.ts
@@ -1,1 +1,13 @@
-export class ShoppingListService {}
+import { Ingredient } from "./../shared/ingredient.model";
+import { Ingredient } from "../shared/ingredient.model";
+
+export class ShoppingListService {
+  ingredients: Ingredient[] = [
+    new Ingredient("Apples", 5),
+    new Ingredient("Tomatoes", 10),
+  ];
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+}
