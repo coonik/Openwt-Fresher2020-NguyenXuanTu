e49fdf015115983c77426f8af43c9f740ceb1563
coonik <vuonglongxungba@gmail.com>
2020-07-14 11:01:59 +0700

    Section 11 - Video 148

--- PROJECT/routing-start/src/app/home/home.component.ts
+++ PROJECT/routing-start/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { AuthServivce } from './../auth.services';
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 
@@ -7,11 +8,19 @@ import { Router } from "@angular/router";
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthServivce) {}
 
   ngOnInit() {}
 
   onLoadServers(id: number) {
     this.router.navigate(["/servers", id, 'edit'], {queryParams: { allowEdit: '1'}, fragment: 'loading'});
+  }
+
+  onLogin() {
+    this.authService.loggedIn = true;
+  }
+
+  onLogout() {
+    this.authService.loggedIn = false;
   }
 }
