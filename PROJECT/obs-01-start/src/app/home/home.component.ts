838bc766d0757c4abc4ac5016e582dfd9f0cc593
coonik <vuonglongxungba@gmail.com>
2020-07-15 09:26:29 +0700

    Section 13 - Video 174

--- PROJECT/obs-01-start/src/app/home/home.component.ts
+++ PROJECT/obs-01-start/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, Subscription, Observable } from 'rxjs';
-import { count } from 'console';
+import { interval, Subscription, Observable, from } from 'rxjs';
+import { map, filter } from 'rxjs/operators'
 
 @Component({
   selector: 'app-home',
@@ -35,7 +35,11 @@ export class HomeComponent implements OnInit, OnDestroy {
       }
     )
 
-    this.sub = customIntervalObservable.subscribe(
+    this.sub = customIntervalObservable.pipe(filter((data)=>{
+      return data>0
+    }),map((data: number) => {
+      return "Round: " + (data+1)
+    })).subscribe(
       (data) => {
         console.log(data);
       }, (error) => {
