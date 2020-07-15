3a5b746748f8fc5e525c7a746de40f7fac9a5221
coonik <vuonglongxungba@gmail.com>
2020-07-15 10:57:39 +0700

    Section 15 - Video 188

--- PROJECT/forms-td-start/src/app/app.component.ts
+++ PROJECT/forms-td-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,12 +7,20 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f')
+  signupForm: NgForm
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(elRef: NgForm) {
-    console.log(elRef.value);
+  // onSubmit(elRef: NgForm) {
+  //   console.log(elRef.value);
+
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm.value);
 
   }
 }
