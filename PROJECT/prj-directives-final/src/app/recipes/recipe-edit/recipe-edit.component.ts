d890480ae37073370f1b7e93aba575e0989d72be
coonik <vuonglongxungba@gmail.com>
2020-07-16 15:02:56 +0700

    Section 16 - Video 230

--- PROJECT/prj-directives-final/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ PROJECT/prj-directives-final/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -55,6 +55,14 @@ export class RecipeEditComponent implements OnInit {
 
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    )
+  }
 
   get ingredientArray(): FormArray { // a getter!
     return this.recipeForm.get('ingredients') as FormArray;
