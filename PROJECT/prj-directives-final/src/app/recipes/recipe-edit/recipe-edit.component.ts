df6a6308a2bf01b64225c73eb942bd72dab0463a
coonik <vuonglongxungba@gmail.com>
2020-07-16 14:48:04 +0700

    Section 16 - Video 229

--- PROJECT/prj-directives-final/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ PROJECT/prj-directives-final/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 import { RecipeService } from '../recipes.service';
 
 @Component({
@@ -30,22 +30,33 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if (recipe['ingredients']) {
+        for ( let item of recipe.ingredients) {
+          recipeIngredients.push(new FormGroup({
+            'name': new FormControl(item.name),
+            'amount': new FormControl(item.amount)
+          }));
+        }
+      }
     }
 
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imagePath': new FormControl(recipeImagePath),
-      'description': new FormControl(recipeDescription)
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients
     })
+
   }
 
-  onSubmit() {
-    console.log(this.recipeForm);
 
+  get ingredientArray(): FormArray { // a getter!
+    return this.recipeForm.get('ingredients') as FormArray;
   }
 }
