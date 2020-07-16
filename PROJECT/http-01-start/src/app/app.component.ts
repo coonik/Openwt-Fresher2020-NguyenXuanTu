6f81e6d12d674d7b94581f757d63c02924faddd7
coonik <vuonglongxungba@gmail.com>
2020-07-16 21:29:28 +0700

    Section 18 - Video 264

--- PROJECT/http-01-start/src/app/app.component.ts
+++ PROJECT/http-01-start/src/app/app.component.ts
@@ -1,21 +1,28 @@
 import { PostsService } from './posts.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   postIsLoading = false;
   error = null;
+  errorSub: Subscription;
 
   constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
+    this.errorSub = this.postsService.error.subscribe(
+      (errorMessage) => {
+        this.error = errorMessage
+      }
+    )
     this.postIsLoading = true;
     this.postsService.fetchPosts()
       .subscribe(
@@ -26,6 +33,10 @@ export class AppComponent implements OnInit {
           this.error = error.message;
         }
       );
+  }
+
+  OnDestroy() {
+    this.errorSub.unsubscribe();
   }
 
   onCreatePost(postData: Post) {
--- PROJECT/http-01-start/src/app/posts.service.ts
+++ PROJECT/http-01-start/src/app/posts.service.ts
@@ -2,9 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
 import { map } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
+  error = new Subject<string>();
   constructor(private http: HttpClient) {};
   createAndStorePost(title: string, content: string) {
     const postData: Post = {title: title, content: content};
@@ -15,6 +17,8 @@ export class PostsService {
       )
       .subscribe(responseData => {
         console.log(responseData);
+      }, error => {
+        this.error.next(error.message);
       });
   }
 
