a831c7b7a1f453b0befe894144bbfd5e561872f4
coonik <vuonglongxungba@gmail.com>
2020-07-16 21:31:46 +0700

    Section 18 - Video 264 - repush

--- PROJECT/http-01-start/src/app/posts.service.ts
+++ PROJECT/http-01-start/src/app/posts.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
