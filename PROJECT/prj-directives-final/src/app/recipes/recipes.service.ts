c73c10d1c5e250d471976e00670a00947e01c502
coonik <vuonglongxungba@gmail.com>
2020-07-17 09:23:40 +0700

    Section 19 - Video 284

--- PROJECT/prj-directives-final/src/app/recipes/recipes.service.ts
+++ PROJECT/prj-directives-final/src/app/recipes/recipes.service.ts
@@ -9,18 +9,18 @@ export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
   currentRecipeIndex: number;
   private recipes: Recipe[] = [
-    new Recipe(
-      "A Test Recipe",
-      "This is simply a test",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
-      [new Ingredient("apple", 12), new Ingredient("coconut", 2)]
-    ),
-    new Recipe(
-      "Another Test Recipe",
-      "This is simply a test",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
-      [new Ingredient("salt", 12), new Ingredient("sweet", 22)]
-    ),
+    // new Recipe(
+    //   "A Test Recipe",
+    //   "This is simply a test",
+    //   "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+    //   [new Ingredient("apple", 12), new Ingredient("coconut", 2)]
+    // ),
+    // new Recipe(
+    //   "Another Test Recipe",
+    //   "This is simply a test",
+    //   "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+    //   [new Ingredient("salt", 12), new Ingredient("sweet", 22)]
+    // ),
   ];
 
   setRecipes(recipes: Recipe[]) {
