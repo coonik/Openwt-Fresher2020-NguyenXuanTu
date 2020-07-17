33fc2c707e6a9ba6c1ca9bd505b8686c9cd969ae
coonik <vuonglongxungba@gmail.com>
2020-07-17 08:32:16 +0700

    Section 19 - Video 280

--- PROJECT/prj-directives-final/src/app/shared/data-storage.service.ts
+++ PROJECT/prj-directives-final/src/app/shared/data-storage.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { RecipeService } from "../recipes/recipes.service";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {
 
+  }
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRepices();
+    this.http
+      .put('https://ng-course-recipe-book-f2d7b.firebaseio.com/recipes.json', recipes)
+        .subscribe(
+          response => {
+            console.log(response);
+
+          }
+        );
+      // return 0;
   }
 }
