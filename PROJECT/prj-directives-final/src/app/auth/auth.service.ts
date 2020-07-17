6cebe7a5008b86919c94a6c6c544c24d4f59ac10
coonik <vuonglongxungba@gmail.com>
2020-07-17 10:37:51 +0700

    Section 20 - Video 292

---
+++ PROJECT/prj-directives-final/src/app/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+interface AuthResponseData {
+  idToken:	string,
+  email: string,
+  refreshToken: string,
+  expiresIn: string,
+  localId: string
+}
+
+@Injectable({ providedIn: 'root'})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC_MI0sjUFVqKvurZtdic80wjVsDFZFVu4',
+      {
+        email,
+        password,
+        returnSecureToke: true
+      }
+    )
+  }
+}
