ddf2809dec9507441c36855055e3a3daac6866b1
coonik <vuonglongxungba@gmail.com>
2020-07-17 10:14:22 +0700

    Section 20 - Video 287

--- PROJECT/prj-directives-final/src/app/app-routing.module.ts
+++ PROJECT/prj-directives-final/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
+import { AuthComponent } from './auth/auth.component';
 
 
 const routes: Routes = [
@@ -17,6 +18,7 @@ const routes: Routes = [
     { path: ":id/edit", component: RecipeEditComponent, resolve: [RecipesResolverService]}
   ] },
   { path: "shopping-list", component: ShoppingListComponent },
+  { path: "auth", component: AuthComponent },
 ];
 
 @NgModule({
