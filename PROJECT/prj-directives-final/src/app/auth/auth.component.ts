dd0c6d378d0a04550831a2af7553baa5f2ad8378
coonik <vuonglongxungba@gmail.com>
2020-07-17 13:50:52 +0700

    Section 20 - Video 296

--- PROJECT/prj-directives-final/src/app/auth/auth.component.ts
+++ PROJECT/prj-directives-final/src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { AuthService } from "./auth.service";
+import { AuthService, AuthResponseData } from "./auth.service";
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-auth',
@@ -10,6 +11,7 @@ export class AuthComponent {
   isLoading = false;
   isLoginMode = false;
   error: string = null;
+  authObs: Observable<AuthResponseData>;
 
   constructor(private authService: AuthService) {}
 
@@ -19,22 +21,26 @@ export class AuthComponent {
 
   onSubmit(form: NgForm) {
     this.isLoading = true;
+    const email = form.value.email;
+    const pass = form.value.password;
 
     if (this.isLoginMode) {
-
+      this.authObs = this.authService.login(email, pass)
     } else {
-      this.authService.signup(form.value.email, form.value.password).subscribe(
-        req => {
-          console.log(req);
-          this.isLoading = false;
-        },
-        tusn => {
-          this.error = tusn;
-          this.isLoading = false;
-        }
-      );
+      this.authObs = this.authService.signup(email, pass);
     }
     console.log(this.isLoading);
+
+    this.authObs.subscribe(
+      req => {
+        console.log(req);
+        this.isLoading = false;
+      },
+      tusn => {
+        this.error = tusn;
+        this.isLoading = false;
+      }
+    );
 
     // form.reset();
   }
--- PROJECT/prj-directives-final/src/app/auth/auth.service.ts
+++ PROJECT/prj-directives-final/src/app/auth/auth.service.ts
@@ -3,12 +3,13 @@ import { HttpClient } from "@angular/common/http";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken:	string,
   email: string,
   refreshToken: string,
   expiresIn: string,
-  localId: string
+  localId: string,
+  register?: boolean
 }
 
 @Injectable({ providedIn: 'root'})
@@ -38,5 +39,16 @@ export class AuthService {
           return throwError(errorMessage);
         })
       )
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC_MI0sjUFVqKvurZtdic80wjVsDFZFVu4',
+      {
+        email,
+        password,
+        returnSecureToke: true
+      }
+    )
+
   }
 }
