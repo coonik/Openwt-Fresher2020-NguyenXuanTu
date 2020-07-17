def38f6e4f5aadafd282dd0514800f93ad484980
coonik <vuonglongxungba@gmail.com>
2020-07-17 14:33:37 +0700

    Section 20 - Video 298

--- PROJECT/prj-directives-final/src/app/auth/auth.service.ts
+++ PROJECT/prj-directives-final/src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { throwError, Subject } from "rxjs";
+import { User } from "./user.model";
 
 export interface AuthResponseData {
   idToken:	string,
@@ -14,6 +15,8 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: 'root'})
 export class AuthService {
+  user = new Subject<User>();
+
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
@@ -26,7 +29,10 @@ export class AuthService {
       }
     )
       .pipe(
-        catchError(this.handleError)
+        catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+        })
       )
   }
 
@@ -61,5 +67,11 @@ export class AuthService {
 
           }
           return throwError(errorMessage);
+  }
+
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
   }
 }
