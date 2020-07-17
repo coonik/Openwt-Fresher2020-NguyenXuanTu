58741b31c8380a44ebd3ccf3e6f1f49d33728e6b
coonik <vuonglongxungba@gmail.com>
2020-07-17 08:55:20 +0700

    Section 19 - Video 282

--- PROJECT/prj-directives-final/src/app/shared/data-storage.service.ts
+++ PROJECT/prj-directives-final/src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators"
 import { RecipeService } from "../recipes/recipes.service";
 import { Recipe } from "../recipes/recipe.model";
 
@@ -24,6 +25,11 @@ export class DataStorageService {
 
   fetchRecipes() {
     this.http.get<Recipe[]>('https://ng-course-recipe-book-f2d7b.firebaseio.com/recipes.json')
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+        })
+      }))
       .subscribe(
         response => {
           this.recipeService.setRecipes(response);
