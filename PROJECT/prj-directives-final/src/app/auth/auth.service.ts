d6e11423f6e856b86400637bbacb91966c807108
coonik <vuonglongxungba@gmail.com>
2020-07-18 20:30:46 +0700

    Section 20 - Video 304

--- PROJECT/prj-directives-final/src/app/auth/auth.service.ts
+++ PROJECT/prj-directives-final/src/app/auth/auth.service.ts
@@ -16,6 +16,7 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: 'root'})
 export class AuthService {
+  private tokenExp: any;
   user = new BehaviorSubject<User>(null);
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -54,6 +55,17 @@ export class AuthService {
   logout() {
     this.user.next(null);
     this.router.navigate(['./auth']);
+    localStorage.removeItem('userData');
+    if (this.tokenExp) {
+      clearTimeout(this.tokenExp);
+    }
+    this.tokenExp = null;
+  }
+
+  autoLogout(expDuration: number) {
+    this.tokenExp = setTimeout(() => {
+      this.logout();
+    }, expDuration);
   }
 
   private handleError(errorRes: HttpErrorResponse) {
@@ -91,6 +103,10 @@ export class AuthService {
     const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate))
     if (loadedUser.token) {
       this.user.next(loadedUser);
+      const expDurationDate = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expDurationDate);
+      console.log(expDurationDate);
+      
     }
   }
 
@@ -98,6 +114,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
     localStorage.setItem('userData', JSON.stringify(user));
   }
 }
