c8c7bfef47df8febbfeb82c14c44a9d401de0828
coonik <vuonglongxungba@gmail.com>
2020-07-31 18:18:46 +0700

    fix token

--- Assignment/Book-Management/src/app/core/services/author.service.ts
+++ Assignment/Book-Management/src/app/core/services/author.service.ts
@@ -5,13 +5,11 @@ import { environment } from './../../../environments/environment';
 @Injectable({providedIn: 'root'})
 export class AuthorService {
 
-  token = "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwidW5pcXVlX25hbWUiOiJtYW5hZ2VyMSIsInJvbGUiOiJNQU5BR0VSIiwibmJmIjoxNTk1ODEyMjY2LCJleHAiOjE1OTYyNDQyNjYsImlhdCI6MTU5NTgxMjI2Nn0.7CiOI-95Vxr6PsrK3-Qo0CaZONrHefist8u8Jf4Wk-rdl68wpabj-qJN7OgdLXRbdIGlU8D3Rg8Cx4hYZuXDGw"
-
   constructor(private http: HttpClient) {}
   getAllAuthor() {
     return this.http.get<any>(environment.apiLink+`/authors`, {
       headers: new HttpHeaders({
-        Authorization: this.token
+        Authorization: 'Bearer ' + environment.token
       }),
     });
   }
@@ -19,7 +17,7 @@ export class AuthorService {
   getAuthorById(id: number) {
     return this.http.get<any>(environment.apiLink+`/authors/${id}`, {
       headers: new HttpHeaders({
-        Authorization: this.token
+        Authorization: 'Bearer ' + environment.token
       }),
     });
   }
