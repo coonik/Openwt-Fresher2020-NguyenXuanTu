cb1a3e2561ac068d9ba8882e769e977de78d450b
coonik <vuonglongxungba@gmail.com>
2020-08-03 11:54:11 +0700

    fix comment

--- Assignment/Book-Management/src/app/core/services/auth-interceptor.service.ts
+++ Assignment/Book-Management/src/app/core/services/auth-interceptor.service.ts
@@ -2,8 +2,6 @@ import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http'
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    console.log("ook");
-
     const modifiedRequest = req.clone({
       headers: req.headers.append('Authorization',localStorage.getItem('token'))
     })
