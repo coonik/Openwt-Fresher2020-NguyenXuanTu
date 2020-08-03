e2dd4ccbfacadd196209dbc352d163928f420b32
coonik <vuonglongxungba@gmail.com>
2020-08-03 16:04:52 +0700

    fix comment

--- Assignment/Book-Management/src/app/app-routing.module.ts
+++ Assignment/Book-Management/src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { NotFoundComponent } from './shared/components/not-found/not-found.compo
 
 const routes: Routes = [
   {path: "", redirectTo: "book", pathMatch: "full"},
-  {path: "book", loadChildren: () => import('./modules/book/book.module').then(m=>m.BookModule)},
-  {path: "user", loadChildren: () => import('./modules/user/user.module').then(m=>m.UserModule)},
-  {path: "author", loadChildren: () => import('./modules/author/author.module').then(m=>m.AuthorModule)},
+  {path: "book", loadChildren: () => import('./modules/book/book.module').then( m => m.BookModule )},
+  {path: "user", loadChildren: () => import('./modules/user/user.module').then( m => m.UserModule )},
+  {path: "author", loadChildren: () => import('./modules/author/author.module').then( m=> m.AuthorModule )},
   {path: '404', component: NotFoundComponent},
   {path: '**', redirectTo: '/404'}
 ];
