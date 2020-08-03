a5a910423110075b658d4b863adc884df0508e87
coonik <vuonglongxungba@gmail.com>
2020-08-03 11:38:47 +0700

    fix comment

--- Assignment/Book-Management/src/app/modules/author/author.module.ts
+++ Assignment/Book-Management/src/app/modules/author/author.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { AuthorEditComponent } from './pages/author-edit/author-edit.component';
 import { AuthorRoutingModule } from "./author-routing.module";
 import { AlifeFileToBase64Module } from 'alife-file-to-base64';
 
@@ -9,8 +8,7 @@ import { AlifeFileToBase64Module } from 'alife-file-to-base64';
     CommonModule,
     AuthorRoutingModule,
     AlifeFileToBase64Module
-  ],
-  declarations: [AuthorEditComponent]
+  ]
 })
 
 export class AuthorModule {}
