ab6cdd9c8bd2641dbab6976e7b27cef3b5e295dd
coonik <vuonglongxungba@gmail.com>
2020-08-11 16:20:35 +0700

    validator login form

--- Assignment/Book-Management/src/app/modules/user/pages/login/login.component.ts
+++ Assignment/Book-Management/src/app/modules/user/pages/login/login.component.ts
@@ -17,6 +17,7 @@ import { debounceTime } from 'rxjs/operators';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   loading: boolean = false;
+  error: string = "";
 
   constructor(private router: Router,
     private userService: UserService,
@@ -34,18 +35,33 @@ export class LoginComponent implements OnInit {
     }
 
     this.loginForm = new FormGroup({
-      username: new FormControl("", [Validators.required]),
-      password: new FormControl("", [Validators.required,Validators.minLength(6)])
+      username: new FormControl("", [Validators.required,Validators.maxLength(30)]),
+      password: new FormControl("", [Validators.required,Validators.minLength(6),Validators.maxLength(30)])
     });
     this.loginForm.valueChanges.pipe(debounceTime(2000)).subscribe( () => {
-        if (!this.loginForm.get("username").valid)
-          this._snackBar.open("Username is required!", "Ok", {
-            duration: 2000,
+        if (!this.loginForm.valid) {
+          let usernameError = this.loginForm.get("username").errors;
+          let passwordError = this.loginForm.get("password").errors;
+          this.error = "";
+          if (usernameError?.required)
+            this.error = "Username is required";
+          if (usernameError?.maxlength) {
+            this.error += this.error === "" ? "Username have at most 30 characters" : null;
+          }
+          if (passwordError?.required)
+            this.error += this.error === "" ? "Password is required" : "; Password is required";
+          else {
+            if (passwordError?.minlength)
+              this.error += this.error === "" ? "Password is include at least 6 characters" : "; Password is include at least 6 characters";
+
+            if (passwordError?.maxlength)
+              this.error += this.error === "" ? "Password have at most 30 characters" : "; Password have at most 30 characters";
+          }
+
+          this._snackBar.open(`${this.error}!`, "Ok", {
+            duration: 5000,
           });
-        if (!this.loginForm.get("password").valid && this.loginForm.get("password").touched)
-          this._snackBar.open("Password is required and Include at least 6 characters!", "Ok", {
-            duration: 2000,
-          });
+        }
       }
     )
   }
@@ -69,7 +85,7 @@ export class LoginComponent implements OnInit {
         this.loginForm.get("password").markAsUntouched();
       });
     } else {
-      this._snackBar.open("Form isn't Valid, Please enter the correct information before login!", "Ok", {
+      this._snackBar.open(`${this.error}!`, "Ok", {
         duration: 5000,
       });
     }
